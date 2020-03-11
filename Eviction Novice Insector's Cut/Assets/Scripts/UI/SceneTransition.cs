7746ee625a9dc9d4e9f66e580b97a788a3f5ec20
SpritersBlock <signupdump1@gmail.com>
2020-03-11 16:12:46 +1300

    Random Scene Transitions

---
+++ Eviction Novice Insector's Cut/Assets/Scripts/UI/SceneTransition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField] Sprite[] maskCollection;
+    [SerializeField] SVGImage mask;
+    [SerializeField] Animator anim;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            CallTransitionCoroutine("hi");
+        }
+    }
+
+    public void CallTransitionCoroutine(string sceneToMoveTo)
+    {
+        StartCoroutine(SceneTransitionCoroutine(sceneToMoveTo));
+    }
+
+    IEnumerator SceneTransitionCoroutine(string sceneToMoveTo)
+    {
+        mask.sprite = maskCollection[Random.Range(0, maskCollection.Length)];
+        anim.SetBool("TransitioningScenes", true);
+
+        yield return new WaitForSeconds(1);
+
+        anim.SetBool("TransitioningScenes", false);
+
+        yield return null;
+    }
+}
