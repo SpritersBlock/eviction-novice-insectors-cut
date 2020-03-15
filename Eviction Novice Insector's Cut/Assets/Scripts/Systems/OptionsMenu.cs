8034ff2f8bb41d2dd3fd383a71d19dccbf182530
SpritersBlock <signupdump1@gmail.com>
2020-03-15 21:06:08 +1300

    Added Options Menu (Sort of)
    Not complete yet

---
+++ Eviction Novice Insector's Cut/Assets/Scripts/Systems/OptionsMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [Header("Audio")]
+    [SerializeField] AudioMixer audioMixer;
+
+    [Header("Quality")]
+    [SerializeField] TMP_Dropdown qualityDropdown;
+
+    private void Start()
+    {
+        SetInitialDropdown();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SetInitialDropdown()
+    {
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
