2286a7481dd94ed1ebaaf1b488ba913fbe749b82
SpritersBlock <signupdump1@gmail.com>
2020-03-23 20:52:34 +1300

    Inventory Deactivates/Reactivates Depending on Scene
    Also added game icon

--- Eviction Novice Insector's Cut/Assets/Scripts/UI/KeepCanvasBetweenScenes.cs
+++ Eviction Novice Insector's Cut/Assets/Scripts/UI/KeepCanvasBetweenScenes.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KeepCanvasBetweenScenes : MonoBehaviour
 {
@@ -17,5 +18,30 @@ public class KeepCanvasBetweenScenes : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+    }
+
+    void OnEnable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        //Debug.Log("Loaded Scene: " + scene.name + ", Mode: " + mode);
+        if (scene.name == "Title Screen")
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
     }
 }
