d5ef1635746fdd797c0d5c2e26d861a9ea59e6d1
SpritersBlock <41931173+SpritersBlock@users.noreply.github.com>
2021-05-15 11:49:12 +1200

    Ant Babies Only Move When Visible, Fixed Redundancy in OpenSceneNames

--- Eviction Novice Insector's Cut/Assets/Scripts/NPCs/AntBaby.cs
+++ Eviction Novice Insector's Cut/Assets/Scripts/NPCs/AntBaby.cs
@@ -7,10 +7,12 @@ public class AntBaby : MonoBehaviour
     Rigidbody rb;
     bool travellingRight;
     [SerializeField] float antSpeed;
+    Renderer r;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        r = GetComponentInChildren<Renderer>();
     }
 
     // Start is called before the first frame update
@@ -23,17 +25,20 @@ public class AntBaby : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.velocity = transform.up * antSpeed;
-        if (travellingRight)
+        if (r.isVisible)
         {
-            transform.localEulerAngles += new Vector3(0, 0, Random.Range(-2, 20));
-        }
-        else if (!travellingRight)
-        {
-            transform.localEulerAngles += new Vector3(0, 0, Random.Range(-20, 2));
-        }
+            rb.velocity = transform.up * antSpeed;
+            if (travellingRight)
+            {
+                transform.localEulerAngles += new Vector3(0, 0, Random.Range(-2, 20));
+            }
+            else if (!travellingRight)
+            {
+                transform.localEulerAngles += new Vector3(0, 0, Random.Range(-20, 2));
+            }
 
-        Invoke("ReverseAntDirection", Random.Range(1,5));
+            Invoke("ReverseAntDirection", Random.Range(1, 5));
+        }
     }
 
     void ReverseAntDirection()
